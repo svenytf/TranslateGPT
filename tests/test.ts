8f7da720b08b9cdbe9898208cdec82c424d39f17
Sven Rubio <sveny@sveny.tf>
2023-09-28 03:23:27 +0200

    tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,8 @@
+//@ts-check
+
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('Index should have "TranslateGPT" h1 displayed', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'TranslateGPT' })).toBeVisible();
 });
